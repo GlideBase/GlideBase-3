676e72287e9ff03126443fa07da091e7861a8f0a
GlideBase <glidebase1@gmail.com>
2025-06-04 15:32:53 +0200

    import raus script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // === Supabase-Konfiguration ===
 
-import { createClient } from '@supabase/supabase-js'Add commentMore actions
 const supabaseUrl = 'https://tzvwghchxzklzcgjqoex.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dndnaGNoeHprbHpjZ2pxb2V4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMzY5NjcsImV4cCI6MjA2NDYxMjk2N30.d_LPinE6_-hQRQX2y-IjSdzZ3oA9nK9pDp0dSlh5-YI'
 const supabase = window.createClient(supabaseUrl, supabaseKey) 
